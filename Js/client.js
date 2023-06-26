279b7eaef00a8d398eeaf08dd7ff2b34d522b244
Hiranmay Mandal <hiranmay1000@gmail.com>
2023-06-26 22:03:22 +0530

    properly created port and linked with the actual url

--- Js/client.js
+++ Js/client.js
@@ -1,5 +1,5 @@
 // const socket = io("http://localhost:8000");
-const socket = io(`https://mychatroom.vercel.app:${server.address().port}`);
+const socket = io("https://mychatroom.vercel.app");
 
 const displayProfileName = document.getElementById('display-profile-name');
 const form = document.getElementById('send-container');
@@ -165,7 +165,7 @@ joinBtn.addEventListener('click', () => {
             append(`${uname} joined the chat`);
         });
         homeContainer.style.display = "none";
-        mainContainer.style.display = "block";
+        mainContainer.style.display = "flex";
         console.log(uname, "is joined");
     }
 })
--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,10 +1,10 @@
-// Node server - Handle socket.io connection
+// // // Node server - Handle socket.io connection
 
-// const server = require('https').createServer();
+// const server = require('http').createServer();
 
-// const io = require('socket.io')(server.address().port, {
+// const io = require('socket.io')(8000, {
 //     cors: {
-//         origin: 'https://mychatroom.vercel.app',
+//         origin: 'http://127.0.0.1:5500',
 //         methods: ['GET', 'POST'],
 //         allowedHeaders: ['Content-Type'],
 //         credentials: true,
@@ -12,27 +12,29 @@
 // });
 
 
-const https = require('https');
-const io = require('socket.io')(https.createServer());
 
-const server = https.createServer();
-
-io.attach(server, {
-  cors: {
-    origin: 'https://mychatroom.vercel.app',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  },
-});
+// server.listen(0, () => {
+//     const port = server.address().port;
+//     console.log(`Server listening on port ${port}`);
+// });
 
 
+const server = require('http').createServer();
+const port = process.env.PORT || 8000; // Use the environment variable for the port or a default value
 
-
-server.listen(0, () => {
-    const port = server.address().port;
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'https://mychatroom.vercel.app', // Replace with your deployed app's URL
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true,
+    },
+});
+
 
 const users = {};
 
