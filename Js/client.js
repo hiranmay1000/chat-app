16475c137733acaecd39ff9d69a6adbd63cf0cd0
Hiranmay Mandal <hiranmay1000@gmail.com>
2023-06-26 22:36:16 +0530

    1.4

--- Js/client.js
+++ Js/client.js
@@ -1,5 +1,5 @@
-// const socket = io("http://localhost:8000");
-const socket = io("https://mychatroom.vercel.app");
+const socket = io("http://localhost:8000");
+// const socket = io("https://mychatroom.vercel.app");
 
 const displayProfileName = document.getElementById('display-profile-name');
 const form = document.getElementById('send-container');
