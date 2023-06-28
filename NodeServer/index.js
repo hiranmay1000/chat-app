9c5a454817c54fb6f0eba66547ae8b08e2d9f944
Hiranmay Mandal <hiranmay1000@gmail.com>
2023-06-28 19:13:03 +0530

    1.7

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,23 +1,5 @@
 // // // Node server - Handle socket.io connection
 
-// const server = require('http').createServer();
-
-// const io = require('socket.io')(8000, {
-//     cors: {
-//         origin: 'http://127.0.0.1:5500',
-//         methods: ['GET', 'POST'],
-//         allowedHeaders: ['Content-Type'],
-//         credentials: true,
-//     },
-// });
-
-
-
-// server.listen(0, () => {
-//     const port = server.address().port;
-//     console.log(`Server listening on port ${port}`);
-// });
-
 
 const server = require('http').createServer();
 const port = process.env.PORT || 8000; // Use the environment variable for the port or a default value
@@ -28,8 +10,8 @@ server.listen(port, () => {
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'https://mychatroom.vercel.app', // Replace with your deployed app's URL
-        // origin: 'http://127.0.0.1:5500',
+        // origin: 'https://mychatroom.vercel.app', // Replace with your deployed app's URL
+        origin: 'http://127.0.0.1:5501',
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type'],
         credentials: true,
@@ -40,22 +22,23 @@ const io = require('socket.io')(server, {
 const users = {};
 
 
+
 io.on('connection', socket => {
     socket.on('new-user-joined', userName => {
         console.log(`New user ${userName} connected.`);
         users[socket.id] = userName;
         socket.broadcast.emit('user-joined', userName);
+        io.emit('active-users', users);
     });
 
     socket.on('chat', message => {
-        console.log("Sender: ", users[socket.id]);
-        console.log("ID: ", socket.id);
         socket.broadcast.emit('receive', { userName: users[socket.id], mess: message, userID: socket.id })
     })
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('leave', users[socket.id]);
         delete users[socket.id];
+        io.emit('disconnected-users', users);
     })
 });
 
